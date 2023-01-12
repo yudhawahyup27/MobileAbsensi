d234a0e2d2e884761abf25c734f367340fdb6593
Nairobi <nairobi@localhost.localdomain>
2023-01-12 11:25:14 +0700

    update

--- app/src/main/java/com/nairobi/tugasakhir/MainActivity.kt
+++ app/src/main/java/com/nairobi/tugasakhir/MainActivity.kt
@@ -2,10 +2,51 @@ package com.nairobi.tugasakhir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.fragment.app.Fragment
+import com.nairobi.tugasakhir.admin.HomepageAdmin
+import com.nairobi.tugasakhir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomepageAdmin())
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomepageAdmin())
+                R.id.absen-> replaceFragment(SplashScreen())
+                R.id.profil -> replaceFragment(HomepageAdmin())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+
+    }
+        private fun replaceFragment(fragment :Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
+
     }
 }
---
+++ app/src/main/java/com/nairobi/tugasakhir/splashscreen.kt
@@ -0,0 +1,19 @@
+package com.nairobi.tugasakhir
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class splashscreen : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+}
